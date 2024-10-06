2d99f95da2eb6b9b2500947e03940e5b423cf477
Neel Kallidai <nl662852@dal.ca>
2024-10-05 21:05:14 -0300

    Initial project

---
+++ src/main/java/ca/dal/treefactor/TreefactorApplication.java
@@ -0,0 +1,13 @@
+package ca.dal.treefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreefactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreefactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/dal/treefactor/TreefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.dal.treefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreefactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
