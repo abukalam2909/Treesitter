270cb37dc1fb2ea5c6281747d357d118526ec9f0
hxu47 <huidongxu1222@gmail.com>
2024-11-05 21:10:13 -0400

    add test for PythonASTVisitor

---
+++ src/test/java/ca/dal/treefactor/PythonASTVisitorTest.java
@@ -0,0 +1,235 @@
+package ca.dal.treefactor;
+
+import ca.dal.treefactor.model.UMLModel;
+import ca.dal.treefactor.model.elements.UMLClass;
+import ca.dal.treefactor.model.elements.UMLOperation;
+import ca.dal.treefactor.model.elements.UMLAttribute;
+import ca.dal.treefactor.model.core.*;
+import ca.dal.treefactor.util.ASTUtil;
+import ca.dal.treefactor.util.PythonASTVisitor;
+import ca.dal.treefactor.util.TreeSitterUtil;
+import io.github.treesitter.jtreesitter.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PythonASTVisitorTest {
+    private Language language;
+    private UMLModel model;
+    private Parser parser;
+    private static final String TEST_FILE = "test.py";
+
+    @BeforeEach
+    void setUp() throws IOException {
+        language = TreeSitterUtil.loadLanguageForFileExtension(TEST_FILE);
+        model = new UMLModel(new HashSet<>(), "python");
+        parser = new Parser();
+        parser.setLanguage(language);
+    }
+
+    @Nested
+    class FunctionTests {
+        @Test
+        void testSimpleFunction() throws Exception {
+            String code = """
+                def greet(name):
+                    print(f"Hello, {name}!")
+                """;
+
+            processCode(code);
+
+            List<UMLOperation> operations = model.getOperations();
+            assertEquals(1, operations.size(), "Should have one function");
+
+            UMLOperation greet = operations.get(0);
+            assertEquals("greet", greet.getName(), "Function name should be 'greet'");
+            assertEquals(Visibility.PUBLIC, greet.getVisibility(), "Should be public by default");
+            assertEquals(1, greet.getParameters().size(), "Should have one parameter");
+            assertEquals("name", greet.getParameters().get(0).getName(), "Parameter should be named 'name'");
+        }
+
+        @Test
+        void testFunctionWithTypeHints() throws Exception {
+            String code = """
+                def greet(name: str) -> str:
+                    return f"Hello, {name}!"
+                """;
+
+            processCode(code);
+
+            UMLOperation greet = model.getOperations().get(0);
+            UMLParameter param = greet.getParameters().get(0);
+
+            assertEquals("str", param.getType().getTypeName(), "Parameter should have str type");
+            assertEquals("str", greet.getReturnType().getTypeName(), "Return type should be str");
+        }
+
+        @Test
+        void testPrivateFunction() throws Exception {
+            String code = """
+                def _helper():
+                    pass
+                """;
+
+            processCode(code);
+
+            UMLOperation helper = model.getOperations().get(0);
+            assertEquals(Visibility.PROTECTED, helper.getVisibility(),
+                    "Function with underscore should be protected");
+        }
+    }
+
+    @Nested
+    class ClassTests {
+        @Test
+        void testSimpleClass() throws Exception {
+            String code = """
+                class Person:
+                    def __init__(self, name):
+                        self.name = name
+                """;
+
+            processCode(code);
+
+            List<UMLClass> classes = model.getClasses();
+            assertEquals(1, classes.size(), "Should have one class");
+
+            UMLClass person = classes.get(0);
+            assertEquals("Person", person.getName(), "Class should be named Person");
+            assertEquals(1, person.getOperations().size(), "Should have one method");
+            assertEquals(1, person.getAttributes().size(), "Should have one attribute");
+        }
+
+        @Test
+        void testClassWithInheritance() throws Exception {
+            String code = """
+                class Animal:
+                    pass
+                    
+                class Dog(Animal):
+                    pass
+                """;
+
+            processCode(code);
+
+            assertEquals(2, model.getClasses().size(), "Should have two classes");
+
+            UMLClass dog = model.getClasses().stream()
+                    .filter(c -> c.getName().equals("Dog"))
+                    .findFirst()
+                    .orElseThrow();
+
+            assertEquals(1, dog.getSuperclasses().size(), "Should have one superclass");
+            assertEquals("Animal", dog.getSuperclasses().get(0), "Superclass should be Animal");
+        }
+
+        @Test
+        void testClassWithDecorator() throws Exception {
+            String code = """
+                @dataclass
+                class Person:
+                    name: str
+                    age: int
+                """;
+
+            processCode(code);
+
+            UMLClass person = model.getClasses().get(0);
+            assertTrue(person.hasAnnotations(), "Should have annotations");
+            assertEquals("dataclass", person.getAnnotations().get(0).getName(),
+                    "Should have dataclass annotation");
+        }
+    }
+
+    @Nested
+    class AttributeTests {
+        @Test
+        void testClassAttributes() throws Exception {
+            String code = """
+                class Counter:
+                    count = 0
+                    _total = 0
+                """;
+
+            processCode(code);
+
+            UMLClass counter = model.getClasses().get(0);
+            List<UMLAttribute> attributes = counter.getAttributes();
+
+            assertEquals(2, attributes.size(), "Should have two attributes");
+
+            UMLAttribute countAttr = attributes.stream()
+                    .filter(a -> a.getName().equals("count"))
+                    .findFirst()
+                    .orElseThrow();
+
+            assertTrue(countAttr.isStatic(), "Should be a static attribute");
+            assertEquals(Visibility.PUBLIC, countAttr.getVisibility(), "Should be public");
+            assertEquals("0", countAttr.getInitialValue(), "Should have initial value");
+        }
+
+        @Test
+        void testInstanceAttributes() throws Exception {
+            String code = """
+                class Person:
+                    def __init__(self):
+                        self.name = "John"
+                        self._age = 30
+                """;
+
+            processCode(code);
+
+            UMLClass person = model.getClasses().get(0);
+            List<UMLAttribute> attributes = person.getAttributes();
+
+            assertEquals(2, attributes.size(), "Should have two attributes");
+            assertTrue(attributes.stream().anyMatch(a ->
+                    a.getName().equals("name") && a.getVisibility() == Visibility.PUBLIC));
+            assertTrue(attributes.stream().anyMatch(a ->
+                    a.getName().equals("_age") && a.getVisibility() == Visibility.PROTECTED));
+        }
+    }
+
+    @Nested
+    class ImportTests {
+        @Test
+        void testSimpleImport() throws Exception {
+            String code = "import datetime";
+
+            processCode(code);
+
+            List<UMLImport> imports = model.getImports(TEST_FILE);
+            assertEquals(1, imports.size(), "Should have one import");
+            assertEquals("datetime", imports.get(0).getImportedName(),
+                    "Should import datetime");
+        }
+
+        @Test
+        void testFromImport() throws Exception {
+            String code = "from os.path import join, dirname";
+
+            processCode(code);
+
+            List<UMLImport> imports = model.getImports(TEST_FILE);
+            assertEquals(2, imports.size(), "Should have two imports");
+            assertTrue(imports.stream().anyMatch(i ->
+                    i.getImportedName().equals("os.path.join")));
+            assertTrue(imports.stream().anyMatch(i ->
+                    i.getImportedName().equals("os.path.dirname")));
+        }
+    }
+
+    // Helper method to process code
+    private void processCode(String code) {
+        try (Tree tree = parser.parse(code, InputEncoding.UTF_8).orElseThrow()) {
+            Node rootNode = tree.getRootNode();
+            PythonASTVisitor visitor = new PythonASTVisitor(model, code, TEST_FILE);
+            ASTUtil.ASTNode astNode = ASTUtil.buildASTWithCursor(rootNode);
+            visitor.visit(astNode);
+        }
+    }
+}
