ac12541c4ad8200936df09f98ecf3bb5014de970
Bishad Deb Roy Utsha <royud11@gmail.com>
2024-11-27 20:17:45 -0400

    cq pipeline updated

--- code_quality/create_issue.py
+++ code_quality/create_issue.py
@@ -4,7 +4,7 @@ import requests
 from csv2md import table
 import csv
 
-def split_list(data, max_rows=150):
+def split_list(data, max_rows=75):
     # Keep headers for each chunk
     headers = data[0]
     chunks = []
